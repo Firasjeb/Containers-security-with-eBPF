f119726262c3e1616af403216fbd891266cd1a6e
firas.jebari <firas.jebari@orange.com>
2023-07-24 13:43:39 +0200

    Fininshed KTLS sniff

--- src/ktls_sniff.c
+++ src/ktls_sniff.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
+#include "ktls_sniff.h"
 //#include <bpf/libbpf.h>
 #include <bpf/bpf.h>
 #include "ktls_sniff.skel.h"
@@ -11,6 +12,10 @@ static volatile bool exiting = false;
 
 void handle_data(void *ctx,int cpu,void *data, __u32 size)
 {
+    struct data *my_data = (struct data*) data;
+    
+    printf(" Size of data : %d , Index of CPU %d  \n",size,cpu);
+    printf("%s\n",my_data -> message);
 
     
     
@@ -20,7 +25,7 @@ void handle_data(void *ctx,int cpu,void *data, __u32 size)
 void lost_data(void *ctx, int cpu , __u64 size)
 {
 
-    
+    fprintf(stderr,"Failed to load data  : DATA LOST \n");
 
 }
 
@@ -36,8 +41,6 @@ int print_libbpf_log(enum libbpf_print_level level,const char* fmt, va_list args
 
 
 int main(int argc, char *argv[]){
-
-    libbpf_set_print(print_libbpf_log);
     
     int err;
     struct perf_buffer* perf;
@@ -46,8 +49,8 @@ int main(int argc, char *argv[]){
     int pid_trace;
     struct ktls_sniff_bpf* skel_ktls;
     int exiting = 0;
-    int zero = 0;
 
+    libbpf_set_print(print_libbpf_log);
 
     skel_ktls = ktls_sniff_bpf__open();
     if(skel_ktls== NULL){
@@ -69,39 +72,52 @@ int main(int argc, char *argv[]){
     }
 
 
-    //fd_prog_ktls = bpf_program__fd(skel_ktls->progs.trace_event_setsockopt);
     fd_map_traced = bpf_map__fd(skel_ktls -> maps.traced_pids);
-    printf("Choose the PID to trace \n");
+    printf("Choose the PID  of process to trace \n");
     scanf("%d",&pid_trace);
     printf("Started tracing the proces %d \n",pid_trace);
-    err = bpf_map_update_elem(fd_map_traced,&zero,&pid_trace,BPF_ANY);
+    err = bpf_map_update_elem(fd_map_traced,&pid_trace,&pid_trace,BPF_ANY);
     if(err){
         fprintf(stderr,"Failed to update map \n");
         goto cleanup;
 
     }
+    printf("Choose the second PID  of process to trace \n");
+    scanf("%d",&pid_trace);
+    err = bpf_map_update_elem(fd_map_traced,&pid_trace,&pid_trace,BPF_ANY);
+    if(err){
+        fprintf(stderr,"Failed to update map \n");
+        goto cleanup;
+    }
 
-    fd_perf_buffer = bpf_map__fd(skel_ktls-> maps.messages);
+    fd_perf_buffer = bpf_map__fd(skel_ktls-> maps.data_tls);
     if(fd_perf_buffer < 0){
         fprintf(stderr,"Failed to find perf buffer");
     }
 
-    perf = perf_buffer__new(fd_perf_buffer,2,handle_data,lost_data,
+    perf = perf_buffer__new(fd_perf_buffer,8,handle_data,lost_data,
                             NULL,NULL);
     err = libbpf_get_error(perf);
 
-    if(perf == NULL){
-        fprintf(stderr,"Failed to open perf buffer");
+    if(err){
+        fprintf(stderr,"Failed to open perf buffer\n");
         goto cleanup;
 
     }
 
-    
     while(!exiting){
-        perf_buffer__poll(perf,100);
+        err  = perf_buffer__poll(perf,200);
+        if(err == -EINTR){
+            err = 0;
+            break;
         }
-    
-    
+
+        if(err < 0 ){
+            printf("error during polling\n");
+            break;
+        }
+        }
+        
     cleanup:
     
     ktls_sniff_bpf__destroy(skel_ktls);
--- src/ktls_sniff.h
+++ src/ktls_sniff.h
@@ -3,15 +3,13 @@
 #define TLS_RX 2
 #define TLS_TX 1
 #define MAX_PROC 20
-#define SIZE_MESSAGE 600 
+#define MAX_DATA_SIZE 20000
 #define TASK_COMM_LEN 16
 
-#define BPF_F_CURRENT_CPU 0xffffffffULL
 
 struct data{
-    int pid;
-    int socket;
-    char message[SIZE_MESSAGE];
+    int len;
+    char message[MAX_DATA_SIZE];
 };
 
 struct socket_info{
